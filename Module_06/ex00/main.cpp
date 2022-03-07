5a0474ae48258c1bd603e9f2854877edc7e99b34
Evangelos Spyromilios <espyromi@c2s17d101.42wolfsburg.de>
2022-03-07 12:32:50 +0100

    main

--- Module_06/ex00/main.cpp
+++ Module_06/ex00/main.cpp
@@ -7,17 +7,8 @@ int		main(int argc, char *argv[]) {
 		std::cout << "Max one argument to be provided" << std::endl;
 		return (-1);
 	}
-	std::string input;
-	input = argv[1];
-
-	Converter *c = new Converter();
-	for (std::string size_type i = 0; i < input.size(); i++) {
-		if ((!std::isalnum(input[i] && (input[i]) != '-') && (!std::isalnum(input[i]) && *input[i] != '+') && (!std::isalnum(input[i]) && *input[i] != '.')) {
-			std::cout << "invalid argument.." << std::endl;
-			return (-1);
-		}
-		Converter::Convert(input);
-		delete c;
+	
+		
 		return (0);
 	} 
 }
